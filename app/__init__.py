892126378420ebc3d9b582570443f9a4d4e1411e
Jason Williams <williamsjacen007@gmail.com>
2020-03-01 13:47:00 -0500

    commited to coding

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
+app.config['SECRET_KEY'] = "whatPassword?"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:superuser@localhost/Lab5"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
+username = "Thriller"  #Not wise as far as security is concerned but I simply wanted to state the username
+password = "cakesoap"  #And the password I used for my database
 db = SQLAlchemy(app)
 
 # Flask-Login login manager
